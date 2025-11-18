c8608631506b299e73b627f8c3ce5c1e66f80b1f
Sushant Kumar <skponpurpose@gmail.com>
2025-11-18 08:35:38 +0530

    after deployment

--- src/Components/testimonials.tsx
+++ src/Components/testimonials.tsx
@@ -145,9 +145,9 @@ export default function Testimonials() {
           >
             <path d="M14.017 21v-7.391c0-5.704 3.731-9.57 8.983-10.609l.995 2.151c-2.432.917-3.995 3.638-3.995 5.849h4v10h-9.983zm-14.017 0v-7.391c0-5.704 3.748-9.57 9-10.609l.996 2.151c-2.433.917-3.996 3.638-3.996 5.849h3.983v10h-9.983z" />
           </svg> */}
-          <h3 className="text-2xl md:text-3xl font-semibold text-white">
+          <h2 className="mb-10 text-3xl md:text-[18px] font-light text-gray-400 tracking-wider">
             What it’s like working with me
-          </h3>
+          </h2>
         </motion.div>
 
        <div className="grid md:grid-cols-2 gap-8">
